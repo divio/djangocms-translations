7cbb5661689dca68cbc12d9208d322dfd1f22b68
Paulo <commonzenpython@gmail.com>
2017-10-10 07:00:31 -0400

    Added aldryn_config

---
+++ aldryn_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from aldryn_client import forms
+
+
+class Form(forms.BaseForm):
+
+    def to_settings(self, data, settings):
+        if 'ALDRYN_SSO_LOGIN_WHITE_LIST' in settings:
+            endpoint = '/admin/djangocms_translations/translationrequest/\w+/callback/'
+            settings['ALDRYN_SSO_LOGIN_WHITE_LIST'].append(endpoint)
