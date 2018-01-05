9a5709acafdaf7c5f3123c78476a84d4a9790dcd
Filipe Waitman <filwaitman@gmail.com>
2018-01-05 17:49:15 -0200

    Fix FIXMEs

--- tests/cms_plugins.py
+++ tests/cms_plugins.py
@@ -1,8 +1,12 @@
+import re
+
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.conf import settings
 
-from djangocms_text_ckeditor.utils import plugin_to_tag, _plugin_tags_to_html, plugin_tags_to_id_list
+from djangocms_text_ckeditor.utils import (
+    OBJ_ADMIN_RE_PATTERN, plugin_to_tag, _plugin_tags_to_html, plugin_tags_to_id_list,
+)
 
 from tests.models import DummyText, DummyLink
 
@@ -25,11 +29,15 @@ class DummyTextPlugin(CMSPluginBase):
 
     @staticmethod
     def get_translation_children_content(content, plugin):
-        import bs4  # FIXME: Try to stick to ckeditor utils.
-        soup = bs4.BeautifulSoup(content, 'html.parser')
+        def _rreplace(text, old, new, count):
+            return new.join(text.rsplit(old, count))
+
+        OBJ_ADMIN_RE_PATTERN_WITH_CONTENT = _rreplace(OBJ_ADMIN_RE_PATTERN, '.*?', '(?P<content>.*?)', 1)
+        data = [x.groups() for x in re.finditer(OBJ_ADMIN_RE_PATTERN_WITH_CONTENT, content)]
+        data = {int(k): v for k, v in data}
 
         return {
-            subplugin_id: soup.find('cms-plugin', id=subplugin_id).text
+            subplugin_id: data[subplugin_id]
             for subplugin_id in plugin_tags_to_id_list(content)
         }
 
