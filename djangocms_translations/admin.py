80ed8e694b59ddc72ff7baf64f820f2ce5f008f6
Filipe Waitman <filwaitman@gmail.com>
2018-02-16 12:47:27 -0200

    Disallow admin delete of translation requests (for sanity)

--- djangocms_translations/admin.py
+++ djangocms_translations/admin.py
@@ -26,6 +26,8 @@ from .utils import get_plugin_form, pretty_json
 
 
 class AllReadOnlyFieldsMixin(object):
+    actions = None
+
     def get_readonly_fields(self, request, obj=None):
         return [
             field.name for field in self.model._meta.get_fields()
